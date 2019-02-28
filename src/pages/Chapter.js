348493c8911e4c9300ec1cc08124c7626b52f198
Manrique Iriarte <m_iriarte33@yahoo.com>
2019-02-27 20:05:53 -0500

    fixed indentation

--- src/pages/Chapter.js
+++ src/pages/Chapter.js
@@ -8,151 +8,147 @@ import {ChProfile} from "../types/ChProfile";
 import { MOCKED_CHAPTER_with_BANNER_and_AVATAR } from '../test/MockedTypes';
 
 export class Chapter extends React.Component {
-    constructor(props) {
-        super(props)
+  constructor(props) {
+    super(props)
 
-        this.state = {
-            loading: 'initial',
-            info: null
-        };
+    this.state = {
+      loading: 'initial',
+      info: null
+    };
+  }
+  async loadChapter() {
+    return DataManager.getItemWithKey(CHAPTER)
+    .catch((error) => {return ("[ERROR - FgProfile > loadFgMember() ]: ", error.message)});
+  }
+  componentDidMount() {
+    this.setState({ loading: 'true' });
+    this.loadChapter()
+    .then( (data) => {
+      //TODO: data is currently null
+      const info = new ChProfile(
+        data.schoolName ? data.schoolName : null,
+        data.chapter ? data.chapter : null,
+        data.bannerSource ? data.bannerSource : null,
+        data.avatarSource ? data.avatarSource : null,
+        data.history ? data.history : null,
+        data.studentAvatars ? data.studentAvatars : null,
+        data.leadershipAvatars ? data.leadershipAvatars : null
+      );
+      //this.setState({member: fgMember, loading: 'false'});
+      this.setState({info, loading: false});
+    })
+    .catch( (error) => {
+      this.setState({info: MOCKED_CHAPTER_with_BANNER_and_AVATAR, loading: false})
+    });
+  }
+
+  render() {
+    if( this.state.loading === 'initial' ) {
+      return <Text>Initializing</Text>
     }
-    async loadChapter() {
-        return DataManager.getItemWithKey(CHAPTER)
-            .catch((error) => {return ("[ERROR - FgProfile > loadFgMember() ]: ", error.message)});
-    }
-    componentDidMount() {
-        this.setState({ loading: 'true' });
-        this.loadChapter()
-            .then( (data) => {
-              console.log(data);
-              console.log(typeof(data));
-              //TODO: data is currently null
-                const info = new ChProfile(
-                    data.schoolName ? data.schoolName : null,
-                    data.chapter ? data.chapter : null,
-                    data.bannerSource ? data.bannerSource : null,
-                    data.avatarSource ? data.avatarSource : null,
-                    data.history ? data.history : null,
-                    data.studentAvatars ? data.studentAvatars : null,
-                    data.leadershipAvatars ? data.leadershipAvatars : null
-                );
-                //this.setState({member: fgMember, loading: 'false'});
-                this.setState({info, loading: false});
-            })
-            .catch( (error) => {
-              console.log(error.message);
-              this.setState({info: MOCKED_CHAPTER_with_BANNER_and_AVATAR, loading: false})
-            });
+
+    if( this.state.loading === 'true' ) {
+      return <Text>Loading</Text>
     }
 
-    render() {
-      console.log(this.state.info);
-        if( this.state.loading === 'initial' ) {
-            return <Text>Initializing</Text>
-        }
+    const bannerHeight = SCREEN_WIDTH * BANNER_HEIGHT_WIDTH_RATIO;
+    return (
+      //Wrap entire profile in a ScrollView
+      <ScrollView
+        style={styles.scrollViewStyle}>
+        <View style={styles.container}>
 
-        if( this.state.loading === 'true' ) {
-            return <Text>Loading</Text>
-        }
+          // Render the Banner
+          <Banner source={this.state.info.bannerSource}/>
 
-        const bannerHeight = SCREEN_WIDTH * BANNER_HEIGHT_WIDTH_RATIO;
-        return (
-            //Wrap entire profile in a ScrollView
-            <ScrollView
-                style={styles.scrollViewStyle}>
-            <View style={styles.container}>
-
-                // Render the Banner
-                <Banner source={this.state.info.bannerSource}/>
-
-                // Render the Avatar
-                <View style={{ top: bannerHeight / 2, flex: 1, marginTop: -170}}>
-                    <Avatar
-                        avatarSize={'large'}
-                        name={this.state.info.schoolName}
-                        source={this.state.info.avatarSource}/>
-                </View>
+          // Render the Avatar
+          <View style={{ top: bannerHeight / 2, flex: 1, marginTop: -170}}>
+            <Avatar
+              avatarSize={'large'}
+              name={this.state.info.schoolName}
+              source={this.state.info.avatarSource}/>
+          </View>
 
 
-                <Text style={[styles.textContainer, { top: bannerHeight * 1.5 }]}>
-                    <Text style={styles.nameLabel}>{this.state.info.schoolName}</Text>{'\n'}
-                    <Text style={styles.gradYearLabel}>{this.state.info.chapter}</Text>{'\n'}
-                </Text>
+          <Text style={[styles.textContainer, { top: bannerHeight * 1.5 }]}>
+            <Text style={styles.nameLabel}>{this.state.info.schoolName}</Text>{'\n'}
+              <Text style={styles.gradYearLabel}>{this.state.info.chapter}</Text>{'\n'}
+              </Text>
 
-                // Render the 'Inspiration' title with horizontal dividers on each side
-                <View style={[styles.inspirationTitle, {top: bannerHeight * 2.345}]}>
-                    <View style={styles.inspirationLine}/>
-                    <Text style={styles.inspirationLabel}>  Our Mission  </Text>
-                    <View style={styles.inspirationLine}/>
-                </View>
+              // Render the 'Inspiration' title with horizontal dividers on each side
+              <View style={[styles.inspirationTitle, {top: bannerHeight * 2.345}]}>
+                <View style={styles.inspirationLine}/>
+                <Text style={styles.inspirationLabel}>  Our Mission  </Text>
+                <View style={styles.inspirationLine}/>
+              </View>
 
-                // Render the member's inspiration block
-                <Text style={[styles.inspirationBlock, {top: bannerHeight * 2.6, marginBottom: 150, paddingBottom: 350 }]}>
-                    {this.state.info.history}
-                </Text>
+              // Render the member's inspiration block
+              <Text style={[styles.inspirationBlock, {top: bannerHeight * 2.6, marginBottom: 150, paddingBottom: 350 }]}>
+                {this.state.info.history}
+              </Text>
             </View>
-            </ScrollView>
+          </ScrollView>
         );
+      }
     }
-}
 
 
-const styles = StyleSheet.create({
-    scrollViewStyle: {
+    const styles = StyleSheet.create({
+      scrollViewStyle: {
         flex: 1,
         opacity: 1,
         top: 0,
         bottom: 0,
         left: 0,
         right: 0,
-    },
-    container: {
+      },
+      container: {
         flex: 1,
         alignItems: 'center',
-    },
-    textContainer: {
+      },
+      textContainer: {
         color: '#818282',
         textAlign: 'center',
         marginTop: -180
-    },
-    nameLabel: {
+      },
+      nameLabel: {
         fontFamily: 'montserrat-light',
         fontSize: 24
-    },
-    schoolLabel: {
+      },
+      schoolLabel: {
         fontFamily: 'open-sans-regular',
         fontSize: 16
-    },
-    gradYearLabel: {
+      },
+      gradYearLabel: {
         fontFamily: 'open-sans-regular',
         fontSize: 14
-    },
-    inspirationTitle: {
+      },
+      inspirationTitle: {
         flex: 1,
         flexDirection: 'row',
         alignContent: 'center',
         justifyContent: 'center',
         marginTop: -100
-    },
-    inspirationLabel: {
+      },
+      inspirationLabel: {
         fontFamily: 'montserrat-regular',
         fontSize: 18,
         color: '#818282'
-    },
-    inspirationLine: {
+      },
+      inspirationLine: {
         // position: 'relative',
         borderBottomColor:'#818282',
         borderBottomWidth:1,
         flex: 1
         // height:'60%',
         // width:'32%'
-    },
-    inspirationBlock: {
+      },
+      inspirationBlock: {
         flex:1,
         fontFamily: 'open-sans-regular',
         fontSize: 14,
         textAlign: 'left',
         color: '#818282',
         margin: 20
-    }
-});
+      }
+    });
