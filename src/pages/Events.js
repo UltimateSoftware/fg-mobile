340716e43bc99b04636c6246350472edfd91b6a1
Juan Suhr <juans@a38753juans.us.corp>
2019-02-28 16:56:45 -0500

    remove calendar and align with UX

--- src/pages/Events.js
+++ src/pages/Events.js
@@ -1,8 +1,9 @@
 import React from 'react';
-import {StyleSheet, View, Text, ScrollView, StatusBar, StatusBarIOS, Image, FlatList} from 'react-native';
+import {StyleSheet, View, Text, ScrollView, StatusBar, StatusBarIOS, Image, FlatList, Picker} from 'react-native';
 import {Header, Left, Right, H2, Button, Card} from 'native-base'
 import {SCREEN_HEIGHT, SCREEN_WIDTH, BANNER_HEIGHT_WIDTH_RATIO } from "../utils/sharedConstants";
 import { Calendar, CalendarList, Agenda } from 'react-native-calendars';
+import { Dropdown } from 'react-native-material-dropdown';
 import {EventService} from "../services/EventService";
 
 const ListView = (props) => {
@@ -43,7 +44,7 @@ const CalendarView = (props) => {
             }}
             onDayPress={(day) => {
                 service = new EventService();
-                props.fgEvents = service.getEvents(day);
+                props.fgEvents = service.getEvents();
             }}
             renderEmptyData={() => {return (<Text> No events </Text>);}}
         />
@@ -52,81 +53,61 @@ const CalendarView = (props) => {
     )
 }
 
-const EventView = () => {
-    return (
-        <View></View>
-    )
-}
-
-const CreateEventBox = () => {
-    return (
-        <View></View>
-    )
-}
-
 export class Events extends React.Component {
     service = new EventService();
     d = new Date();
+    monthList = ['Jan','Feb','Mar','Apr','May','Jun','Jul','Aug','Sep','Oct','Nov','Dec']
 
     constructor(props) {
         super(props);
         this.state = {
-            listMode: false,
-            selector: require('./Events-assets/list-view.png'),
-            fgEvents: []
+            fgEvents: [],
+            currMonth: this.monthList[0][this.d.getMonth()],
+            isLoading: true
         };
     }
 
     async componentWillMount() {
-        loadedData = await this.service.getEvents(this.d.getFullYear() + '-' + ("0" + (this.d.getMonth() + 1)).slice(-2) + '-' + ("0" + this.d.getDate()).slice(-2)) //current date formatted
+        loadedData = await this.service.getEvents()
         this.setState({
-            fgEvents: loadedData
+            fgEvents: loadedData,
+            isLoading: false
         });
     }
 
-    handleToggle = async () => {
-        this.setState(() => ({ listMode: !this.state.listMode}));
-        
-        if(this.state.listMode) {
-            loadedData = await this.service.getEvents(this.d.getFullYear() + '-' + ("0" + (this.d.getMonth() + 1)).slice(-2) + '-' + ("0" + this.d.getDate()).slice(-2)) //current date formatted
-            this.setState({
-                selector: require('./Events-assets/list-view.png'),
-                fgEvents: loadedData
-            });
-        } 
-        else {
-            loadedData = await this.service.getEvents()
-            this.setState({
-                selector: require('./Events-assets/grid-view.png'),
-                fgEvents: loadedData
-            });
-        }
-        console.log(this.state.fgEvents)
-    }
-
     render() {
-        return (
-            <View style={{height: SCREEN_HEIGHT*.93, width: SCREEN_WIDTH, alignItems: 'center'}}>
-            <Header style={{width:SCREEN_WIDTH, height: SCREEN_HEIGHT*.125}}> 
-                <Left>
-                    <H2 style={{fontFamily: 'montserrat-bold', color: '#818282', paddingLeft: 20}}>Events</H2>
-                </Left>
-                <Right style={{alignItems:'center'}}>
-                    <Button transparent style={{paddingRight: 30}} onPress={() => console.log(this.state.listMode)}>
-                        <Image style={{width: 38, height: 38}} source={require('./Events-assets/plus_button.png')}/>
-                    </Button>
-                </Right>
-            </Header>
-            <View style={{flexDirection: 'row', marginLeft: SCREEN_WIDTH*.76, marginTop: 4}}>
-                <Button transparent style={{padding: 10}} onPress={() => this.handleToggle(true)}>
-                    <Image style={{width: 30, height: 30}} source={this.state.selector}/>
-                </Button>
-            </View>
-            <View>
-                {(this.state.listMode) ? <ListView fgEvents={this.state.fgEvents}/> : <CalendarView/> }
-            </View>
-            </View>
-        )
+        console.log(this.state.currMonth)
+
+        if(!this.state.isLoading) {
+            return (
+                <View style={{height: SCREEN_HEIGHT*.93, width: SCREEN_WIDTH, justifyContent: 'space-between'}}>
+                <Header style={{width:SCREEN_WIDTH, height: SCREEN_HEIGHT*.125, borderBottomWidth: 2}}> 
+                    <Left>
+                        <H2 style={{fontFamily: 'montserrat-bold', color: '#818282', paddingLeft: 20, fontSize: 24}}>Events</H2>
+                    </Left>
+                    <Right style={{alignItems:'center'}}>
+                        <Button transparent style={{paddingRight: 30}}>
+                            <Image style={{width: 38, height: 38}} source={{uri:'https://banner2.kisspng.com/20180418/jjq/kisspng-spotify-computer-icons-paypal-5ad6d7b8ef88e3.0461224615240293689811.jpg'}}/>
+                        </Button>
+                    </Right>
+                </Header>
+                <View style={{borderWidth: 1, minHeight: '5%', width: 400, marginTop: '1%'}}>
+                    <Dropdown
+                        label={this.state.currMonth}
+                        data={this.monthList}
+                    />
+                </View>
+                <View style={{borderWidth: 1, height: 200, width: 400}}>
+                    List current date
+                </View>
+                <View style={{borderWidth: 1, height: 200, width: 400}}>
+                    List events
+                </View>
+                </View>
+            )
+        } else {
+            return <View></View>
+        }
     }
 }
 
--- src/services/EventService.js
+++ src/services/EventService.js
@@ -2,7 +2,8 @@ import {FgEvent} from "../types/FgEvent";
 
 export class EventService {
 
-    async getEvents(date = null) {
+    /*
+    async getEvents(month = null) {
       var append
       if(date === null) append = ''; //all dates
       else              append = '?exactDate=' + date; //exact date
@@ -17,5 +18,69 @@ export class EventService {
               resolve(new FgEvent('', '', '', '', '', '', ['']))
           }
       })
+    }
+    */
+
+    async getEvents(date = null) {
+        return (
+            [
+                {
+                    "date": "2019-03-03",
+                    "chapterId": "17cbecac-dc59-46b1-93ad-faf89cda7268",
+                    "description": "Description of the hangout and all its fun stuff.",
+                    "location": "Phase 10",
+                    "id": "d998ded4-0efc-437e-a3f9-1ef07bdbe0ef",
+                    "state": "Unpublished",
+                    "title": "test Hangout",
+                    "icebreakers": [
+                        "Icebreaker Numero Uno"
+                    ]
+                },{
+                    "date": "2019-03-03",
+                    "chapterId": "17cbecac-dc59-46b1-93ad-faf89cda7268",
+                    "description": "Description of the hangout and all its fun stuff.",
+                    "location": "Phase 10",
+                    "id": "d998ded4-0efc-437e-a3f9-1ef07bdbe0ef",
+                    "state": "Unpublished",
+                    "title": "test Hangout",
+                    "icebreakers": [
+                        "Icebreaker Numero Uno"
+                    ]
+                },{
+                    "date": "2019-03-03",
+                    "chapterId": "17cbecac-dc59-46b1-93ad-faf89cda7268",
+                    "description": "Description of the hangout and all its fun stuff.",
+                    "location": "Phase 10",
+                    "id": "d998ded4-0efc-437e-a3f9-1ef07bdbe0ef",
+                    "state": "Unpublished",
+                    "title": "test Hangout",
+                    "icebreakers": [
+                        "Icebreaker Numero Uno"
+                    ]
+                },{
+                    "date": "2019-03-03",
+                    "chapterId": "17cbecac-dc59-46b1-93ad-faf89cda7268",
+                    "description": "Description of the hangout and all its fun stuff.",
+                    "location": "Phase 10",
+                    "id": "d998ded4-0efc-437e-a3f9-1ef07bdbe0ef",
+                    "state": "Unpublished",
+                    "title": "test Hangout",
+                    "icebreakers": [
+                        "Icebreaker Numero Uno"
+                    ]
+                },{
+                    "date": "2019-03-03",
+                    "chapterId": "17cbecac-dc59-46b1-93ad-faf89cda7268",
+                    "description": "Description of the hangout and all its fun stuff.",
+                    "location": "Phase 10",
+                    "id": "d998ded4-0efc-437e-a3f9-1ef07bdbe0ef",
+                    "state": "Unpublished",
+                    "title": "test Hangout",
+                    "icebreakers": [
+                        "Icebreaker Numero Uno"
+                    ]
+                },
+            ]
+        )
     }
 }
