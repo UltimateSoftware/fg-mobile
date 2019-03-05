73b108f6f6718a995c3cc58387e83eac1b0e2fe8
Kaden King <kaden_king@ultimatesoftware.com>
2019-03-04 21:24:17 -0500

    hangouts redesigned and integrated with the back end

--- src/pages/HangoutLanding.js
+++ src/pages/HangoutLanding.js
@@ -8,91 +8,99 @@ import { Header, Left, Button, Body, Right, Title, Card, H2, CardItem, Thumbnail
 
 import {SCREEN_HEIGHT, SCREEN_WIDTH, BANNER_HEIGHT_WIDTH_RATIO } from "../utils/sharedConstants";
 import {Banner} from "../components/Banner";
+import {HangoutService} from '../services/HangoutService';
 import { black } from 'ansi-colors';
 
 //TODO: Create AvatarGroup component to display chapter sisters.
 //TODO: Create FgButton to allow 'View All' click to see all chapter sisters.
 //TODO: Add ChapterSisters section below Inspiration Block
 //TODO: Status bar background should be white not translucent
-data = [
-    {key: '1', id: 'Pot Luck', date: 'January, 10, 2019'},
-    {key: '2', id: 'Jackson Memorial', date: 'Feburary 10, 2019'},
-    {key: '3', id: 'James Albright Foundation', date: 'March 10, 2019'},
-    {key: '4', id: 'Starbucks MeetUp', date: 'April 10, 2019'},
-    {key: '5', id: 'JFK Library', date: 'May 10, 2019'},
-    {key: '6', id: 'Jillian Fitness', date: 'June 10, 2019'},
-    {key: '7', id: 'Jimmy Kimell Watch Party', date: 'July 10, 2019'},
-    {key: '8', id: 'Julie\'s Birhtday', date: 'August 10, 2019'},
-    {key: '9', id: 'Ronald McDonald Foundation', date: 'September 10, 2019'},
-    {key: '10', id: 'Pot Luck', date: 'January, 10, 2019'},
-    {key: '11', id: 'Jackson Memorial', date: 'Feburary 10, 2019'},
-    {key: '12', id: 'James Albright Foundation', date: 'March 10, 2019'},
-    {key: '13', id: 'Starbucks MeetUp', date: 'April 10, 2019'},
-    {key: '14', id: 'JFK Library', date: 'May 10, 2019'},
-  ];
 
-function checkIsEven(n) {
-    if(n%2 == 0) {
-        return true;
-    }
-    return false;
-}
+const icons = {
+    "book": (<Image style={{height:100, width:100, display: 'flex'}} source={require('../../assets/hangout_icons/book.png')}/>),
+    "coffee": (<Image style={{height:100, width:100, display: 'flex'}} source={require('../../assets/hangout_icons/coffee.png')}/>),
+    "food": (<Image style={{height:100, width:100, display: 'flex'}} source={require('../../assets/hangout_icons/food.png')}/>)
+};
 
-const ListOfHangouts = () => {return (
-<ScrollView
-                style={styles.scrollViewStyle}
-                bounces={false}
-            >
- 
-            <View style={styles.container}>
-            {data.map((r, i) => 
-                        <View style={styles.shadowBox}>
-                        <View key={i} style = {[styles.item, {marginRight: checkIsEven(i) ? SCREEN_WIDTH*.025 : 0}]}>
-                             <Image style={{height:100, width:100, display: 'flex'}} source={{uri: 'http://via.placeholder.com/100'}}/>
-                             <Text style={{marginTop: 10, textAlign: 'center', color: 'rgba(0, 0, 0, 0.84)'}}>{r.id}</Text>
-                        </View>
-                        </View>
-                )}
-        }
-            </View>
-            </ScrollView>
-);}
-const IceBreakers= () =>{
-    return (
-        <ScrollView
-                style={[styles.scrollViewStyle]}
-                bounces={false}
-                flexDirection='row'
-            >
- 
-            <View style={styles.container}>
-            <FlatList
-            numColumns={2}
-            bounces={false}
-            style={{alignContent:'center', }}
-            data={[
-                {key: 'Devin', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
-                {key: 'Jackson', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
-                {key: 'James', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
-                {key: 'Joel', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
-                {key: 'John', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
-                {key: 'Jillian', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
-            ]}
-            renderItem={({item}) => <TouchableOpacity style={[styles.item,{borderRadius:10, padding:40, shadowRadius: 5, }]}>
-            <Card rounded style={{flex: 1, shadowOpacity: 10, margin: 10, borderRadius: 10, alignItems: 'center' }}>
-                    <Image source={{uri:item.img}} style={{width:35, height: 35}}/>
-                <CardItem footer bordered>
-                    <Text style={[{height:18}]}>{item.key}</Text>
-                </CardItem>
-            </Card></TouchableOpacity>}
-            />
-
-            </View>
-            </ScrollView>
-        
-    );
-}
 export class HangoutLanding extends React.Component {
+    constructor() {
+        super();
+        this.state = {
+            hangouts: [],
+        }
+        this.HangoutService = new HangoutService();
+    }
+
+    componentDidMount() {
+        this.HangoutService.getHangouts().then((data) => {
+            this.setState({hangouts: data});
+        });
+    }
+
+     checkIsEven(n) {
+        if(n%2 == 0) {
+            return true;
+        }
+        return false;
+    }
+
+    getImage(icon) {
+        
+    }
+    
+     IceBreakers= () =>{
+        return (
+            <ScrollView
+                    style={[styles.scrollViewStyle]}
+                    bounces={false}
+                    flexDirection='row'
+                >
+     
+                <View style={styles.container}>
+                <FlatList
+                numColumns={2}
+                bounces={false}
+                style={{alignContent:'center', }}
+                data={[
+                    {key: 'Devin', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
+                    {key: 'Jackson', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
+                    {key: 'James', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
+                    {key: 'Joel', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
+                    {key: 'John', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
+                    {key: 'Jillian', img: 'https://icon2.kisspng.com/20180320/sdq/kisspng-computer-icons-alarm-clocks-clip-art-clock-icons-no-attribution-5ab0ba010aeb80.8431035315215313930447.jpg'},
+                ]}
+                renderItem={({item}) => <TouchableOpacity style={[styles.item,{borderRadius:10, padding:40, shadowRadius: 5, }]}>
+                <Card rounded style={{flex: 1, shadowOpacity: 10, margin: 10, borderRadius: 10, alignItems: 'center' }}>
+                        <Image source={{uri:item.img}} style={{width:35, height: 35}}/>
+                    <CardItem footer bordered>
+                        <Text style={[{height:18}]}>{item.key}</Text>
+                    </CardItem>
+                </Card></TouchableOpacity>}
+                />
+    
+                </View>
+                </ScrollView>
+            
+        );
+    }
+
+    ListOfHangouts = () => {return (
+        <ScrollView
+                        style={styles.scrollViewStyle}
+                        bounces={false}
+                    >
+        
+                    <View style={styles.container}>
+                    {this.state.hangouts.map((r, i) => 
+                                <View key={i} style = {[styles.item, {marginRight: this.checkIsEven(i) ? SCREEN_WIDTH*.025 : i}, {marginTop: (i == 0 || i == 1) ? SCREEN_HEIGHT*0.03 : 0}]}>
+                                     {icons[r.icon]}
+                                     <Text style={{marginTop: 10, textAlign: 'center', color: 'rgba(0, 0, 0, 0.84)'}}>{r.title}</Text>
+                                </View>
+                        )}
+                }
+                    </View>
+                    </ScrollView>
+        );}
 
     render() {
         const bannerHeight = SCREEN_WIDTH * BANNER_HEIGHT_WIDTH_RATIO;
@@ -111,12 +119,11 @@ export class HangoutLanding extends React.Component {
                 </Right>
             </Header>
             <Banner text='Hangouts' source='https://d31l02nbp0owar.cloudfront.net/m/t/198/1977194/a-0120.jpg' />
-                {<ListOfHangouts></ListOfHangouts>}
+                {<this.ListOfHangouts></this.ListOfHangouts>}
 
             </View>
         );
     }
-
 }
 
 const styles = StyleSheet.create({
@@ -155,13 +162,11 @@ const styles = StyleSheet.create({
         width: SCREEN_WIDTH*.4,
         borderColor: 'rgba(58, 106, 117, 0.18)',
         borderWidth: 1,
-        marginBottom: SCREEN_HEIGHT*.03,
+        marginBottom:SCREEN_HEIGHT*.04,
         borderRadius : 20,
-    },
-    shadowBox: {
-		shadowColor: "#000", 
+        shadowColor: "#000", 
 		shadowOffset: { width: 1, height: 5 }, 
-		shadowOpacity: 0.08, 
+		shadowOpacity: 0.15, 
 		shadowRadius: 10,
         elevation: 10,
     },
---
+++ src/types/Hangout.js
@@ -0,0 +1,8 @@
+export function Hangout (title, location, description, icebreakers, state, icon) {
+    this.title = title;
+    this.location = location;
+    this.description = description;
+    this.icebreakers = icebreakers;
+    this.state = state;
+    this.icon = icon;
+}
