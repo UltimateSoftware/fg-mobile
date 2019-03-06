1482899bf6fcc654b3776fb3567c60b2b5b8089e
Grethell Martinez <grethellm@a45393grethellm.us.corp>
2019-03-06 13:11:44 -0500

    starting backend integration

--- src/services/FgProfileService.js
+++ src/services/FgProfileService.js
@@ -4,7 +4,7 @@ import {DataManager, SIGNED_IN_MEMBER, SIGNED_IN_MEMBER_ID} from "../DataManager
 export class FgProfileService {
 
     createMember(member) {
-        return fetch('http://localhost:5000/api/v1/profile/', {
+        return fetch('http://localhost:5000/api/v1/profiles/', {
             method: 'POST',
             headers: {
                 Accept: 'application/json',
@@ -25,8 +25,26 @@ export class FgProfileService {
             .catch( (error) => console.log(error.message));
     }
 
-    updateMember(member) {
-        // TODO
+    updateMember(member, id) {
+        return fetch('http://localhost:5000/api/v1/profiles/' + id, {
+            method: 'PUT',
+            headers: {
+                Accept: 'application/json',
+                'Content-Type': 'application/json',
+            },
+            body: JSON.stringify({
+                firstName: member.firstName ? member.firstName : null,
+                lastName: member.lastName ? member.lastName : null,
+                schoolName: member.schoolName ? member.schoolName : null,
+                gradYear: member.gradYear ? member.gradYear : null,
+                inspiration: member.inspiration ? member.inspiration : null
+            })
+        })
+            .then((response) => response.json())
+            .then((data) => {
+                return data.id;
+            })
+            .catch( (error) => console.log(error.message));
     }
 
 }
