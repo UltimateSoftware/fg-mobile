858894bf561038ff8867eec75b9ac66897ef759f
Jonathan Beltran <jbelt021@fiu.edu>
2019-03-14 12:56:25 -0400

    Updated HangoutService.js
    switch to async syntax

--- src/services/HangoutService.js
+++ src/services/HangoutService.js
@@ -1,14 +1,19 @@
 import {Hangout} from "../types/Hangout";
 
 export class HangoutService {
-    getHangouts() {
-        return fetch("http://localhost:5000/hangouts")
-        .then((response) => response.json())
-        .then((responseJson) => {
-            var data = responseJson.map((response) => new Hangout(response.title, response.location, response.content, response.icebreakers, response.state));
-            return data;
-        })
-        .catch(console.error);
+    async getHangouts() {
+        
+        return new Promise((resolve, error) => {
+            try {
+                var request = await fetch("http://localhost:5000/hangouts");
+                var response = await response.json();
+                var data = responseJson.map((response) => new Hangout(response.title, response.location, response.content, response.icebreakers, response.state));
+                resolve(data);
+            }
+            catch (e) {
+                resolve(e);
+            }
+        });
     }
 
 }
