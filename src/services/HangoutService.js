a5e845c7de02488f4b03242a39426dca2f76e371
Kaden King <kaden_king@ultimatesoftware.com>
2019-03-14 13:00:24 -0400

    hangout service typo fix

--- src/services/HangoutService.js
+++ src/services/HangoutService.js
@@ -1,17 +1,18 @@
 import {Hangout} from "../types/Hangout";
+// import console = require("console");
 
 export class HangoutService {
     async getHangouts() {
         
-        return new Promise((resolve, error) => {
+        return new Promise(async (resolve, error) => {
             try {
                 var request = await fetch("http://localhost:5000/hangouts");
-                var response = await response.json();
-                var data = responseJson.map((response) => new Hangout(response.title, response.location, response.content, response.icebreakers, response.state));
+                var response = await request.json();
+                var data = response.map((response) => new Hangout(response.title, response.location, response.content, response.icebreakers, response.state));
                 resolve(data);
             }
             catch (e) {
-                resolve(e);
+                console.log(e);
             }
         });
     }
