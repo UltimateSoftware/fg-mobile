e696bb7a916463d189ae002360a1edbce060d129
Ben Shapiro <btqshapiro@gmail.com>
2019-03-17 11:19:49 -0400

    use context api

--- src/Router.js
+++ src/Router.js
@@ -1,6 +1,7 @@
 import {createBottomTabNavigator, createStackNavigator, createSwitchNavigator} from "react-navigation";
-
-import {CreateProfile } from "./pages/CreateProfile";
+import {LoadAuth} from "./pages/LoadAuth";
+import {LoadProfile} from "./pages/LoadProfile";
+import {CreateProfile} from "./pages/CreateProfile";
 import {Login} from "./pages/Login";
 import {Menu} from "./pages/Menu";
 import {Chapter} from "./pages/Chapter";
@@ -12,65 +13,74 @@ import {CreateChapter} from "./pages/CreateChapter"
 import Ionicons from "react-native-vector-icons/Ionicons";
 import React from "react";
 
-export const createRootNavigator = (signedIn = false) => {
-    return createSwitchNavigator(
-        {
-            SignedIn: { screen: SignedInNavigator },
-            SignedOut: { screen: SignedOutNavigator }
-        },
-        {
-            initialRouteName: signedIn ? "SignedIn" : "SignedOut"
-        }
-    );
-};
+export const SignedIn =  createBottomTabNavigator(
+{
+    Menu: Menu,
+    MyChapter: Chapter,
+    Hangouts: HangoutLanding,
+    Events: Events,
+    Profile: FgProfile,
+},
+{
+    initialRouteName: 'Profile',
+    navigationOptions: ({ navigation }) => ({
+        tabBarIcon: ({ focused, horizontal, tintColor }) => {
+            const { routeName } = navigation.state;
+            let iconName;
+            if (routeName === 'Profile') {
+                iconName = `md-person`;
+            } else if (routeName === 'Hangouts') {
+                iconName = `md-people`;
+            } else if (routeName === 'Events') {
+                iconName = `md-calendar`
+            } else if (routeName === 'Menu') {
+                iconName = `md-menu`
+            } else if (routeName === 'MyChapter') {
+                iconName = `md-bonfire`
+            }
 
-export const SignedOutNavigator = createStackNavigator(
+            // You can return any component that you like here! We usually use an
+            // icon component from react-native-vector-icons
+            return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
+            // return <Image source={{ uri: iconName}} style={{height: 30, width: 30}} />; // size={horizontal ? 20 : 25} color={tintColor}
+        },
+    }),
+    tabBarOptions: {
+        activeTintColor: 'tomato',
+        inactiveTintColor: 'gray',
+    },
+}
+);  
+
+export const SignedOut = createStackNavigator(
     {
         SignIn: { screen: Login },
         SignUp: { screen: CreateProfile },
-        SelectChapter: { screen: ChapterSelector },
-        CreateChapter: { screen: CreateChapter }
+        App: SignedIn
     },
     {
-        initialRouteName: 'SignIn'
+        initialRouteName: 'SignIn',
     }
 );
 
-export const SignedInNavigator =  createBottomTabNavigator(
+export const ProfileController = createSwitchNavigator(
     {
-        Menu: Menu,
-        MyChapter: Chapter,
-        Hangouts: HangoutLanding,
-        Events: Events,
-        Profile: FgProfile
+      LoadingProfile: LoadProfile,
+      SignUp: { screen: CreateProfile },
+      App: SignedIn
     },
     {
-        initialRouteName: 'Profile',
-        navigationOptions: ({ navigation }) => ({
-            tabBarIcon: ({ focused, horizontal, tintColor }) => {
-                const { routeName } = navigation.state;
-                let iconName;
-                if (routeName === 'Profile') {
-                    iconName = `md-person`;
-                } else if (routeName === 'Hangouts') {
-                    iconName = `md-people`;
-                } else if (routeName === 'Events') {
-                    iconName = `md-calendar`
-                } else if (routeName === 'Menu') {
-                    iconName = `md-menu`
-                } else if (routeName === 'MyChapter') {
-                    iconName = `md-bonfire`
-                }
-
-                // You can return any component that you like here! We usually use an
-                // icon component from react-native-vector-icons
-                return <Ionicons name={iconName} size={horizontal ? 20 : 25} color={tintColor} />;
-                // return <Image source={{ uri: iconName}} style={{height: 30, width: 30}} />; // size={horizontal ? 20 : 25} color={tintColor}
-            },
-        }),
-        tabBarOptions: {
-            activeTintColor: 'tomato',
-            inactiveTintColor: 'gray',
-        },
+      initialRouteName: 'LoadingProfile',
     }
-);
+  );
+
+  export const AppController = createSwitchNavigator(
+    {
+      AuthLoading: LoadAuth,
+      ProfileLoading: ProfileController,
+      Auth: SignedOut,
+    },
+    {
+      initialRouteName: 'AuthLoading',
+    }
+  );
