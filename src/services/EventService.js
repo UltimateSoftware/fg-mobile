362f465ac5f40a14782f85f516d968735615295a
Juan Suhr <juansuhr@Juans-MacBook-Air.local>
2019-03-18 13:02:34 -0400

    no data handling

--- src/services/EventService.js
+++ src/services/EventService.js
@@ -3,19 +3,20 @@ import {FgEvent} from "../types/FgEvent";
 export class EventService {
 
     
-    async getEvents(month = null) {
+    async getEvents(date = null) {
       var append
       if(date === null) append = ''; //all dates
       else              append = '?exactDate=' + date; //exact date
         return new Promise(async (resolve) => {
           try {
+              console.log('yeet')
               var result = await fetch('http://localhost:5000/hangouts' + append, {
                   method: 'GET'
               })
               resolve(result.json())
           } catch (e) {
               console.log('error been caught')
-              resolve(new FgEvent('', '', '', '', '', '', ['']))
+              resolve(new FgEvent('', '', '2010-05-05', '', '', '', ['']))
           }
       })
     }
