2323a4e6f97fbd87d808292b4c3d25ded5474e63
Jonathan Beltran <jonathanbeltran@Jonathans-MacBook-Pro.local>
2019-06-04 12:44:10 -0400

    Added example service workflow

--- src/pages/FgChapter.js
+++ src/pages/FgChapter.js
@@ -1,43 +1,23 @@
 import React, {Component, useState, useEffect} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
+import {Platform, StyleSheet, Text, View, Button} from 'react-native';
 import HamburgerIcon from '../components/primatives/HamburgerIcon';
 import {Inspiration} from '../components/atoms/Inspiration';
-import {useIsLoading} from '../hooks/useIsLoading';
 import {Banner} from '../components/atoms/Banner';
 import {ProfileFrame} from '../components/primatives/ProfileFrame';
-import {ChapterManager} from '../domain/models/Chapter'
+import useChapter from '../domain/models/Chapter';
 
-function testIsLoading(){
-    return new Promise((resolve) => {
-        setTimeout(() => {resolve(true)}, 2000)
-    });
+function oneOff(){
+
 }
 
 function FgChapter() {
-    useEffect(() => {
-        (async () => {
-            let loadChapter = await ChapterManager.getChProfile();
-            updateChapter(loadChapter);
-        })();
-        return () => {
-        };
-    }, [])
 
-    const [chapter, updateChapter] = useState({})
-
-    async function _loadChapter() {
-        return new Promise(async (resolve) => {
-            let loadChapter = await ChapterManager.getChProfile();
-            // error handle the failure to load chapter
-            
-            console.log('updating chapter');
-            updateChapter(loadChapter);
-            resolve(true);
-        });
-    };
-
+    const [chapter, chapterActions] = useChapter()
+    const {Chapter, Status} = chapter;
+    console.log(Status);
+    console.log(chapterActions)
+    
     const imgUri = 'fearlesslyGirl_logo.jpg';
-    const isLoading = useIsLoading(_loadChapter)
     
     return (
         <View style={styles.container}>
@@ -45,7 +25,8 @@ function FgChapter() {
                 <ProfileFrame source={imgUri} size={'l'}/>
             </Banner>
             <Inspiration title={"my title"} inspiration={"some text"}/>
-            {<Text >{isLoading ? chapter.thisField : 'loading'}</Text> }
+            <Button title="something" onPress={() => {chapterActions.updateChapter()}}/>
+            {<Text >{Status == 'READY' ? Chapter.schoolName : 'loading'}</Text> }
         </View>
     );
 }
