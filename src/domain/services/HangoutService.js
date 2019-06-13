e74d60ce0371e3816a4a45e058ab0b0ae1ad7941
Jonathan Beltran <jonathanbeltran@Jonathans-MacBook-Pro.local>
2019-06-13 11:48:30 -0400

    added API Base

--- src/domain/services/HangoutService.js
+++ src/domain/services/HangoutService.js
@@ -1,17 +1,20 @@
 import {status} from '../constants/Hangout';
+import {API_BASE} from '../../SharedConstants';
 
-
-export const createHangout = store => {
-    store.setState({ Status: status.init });
+export const createHangout = async store => {
+    store.setState({ Status: status.loading });
     // perform hangout creation
-    store.setState({ Status: status.ready });
+    return new Promise((resolve, reject) => {
+        store.setState({ Status: status.ready });
+        resolve();
+    });
 }
 
-export const loadHangouts = async(store) => {
+export const loadHangouts = async store => {
     store.setState({ Status: status.loading });
     // call Chapter API
     return new Promise((resolve, reject) => {
-        store.setState({ Status: status.loading });
+        store.setState({ Status: status.ready });
         resolve();
     });
 
--- src/domain/services/ProfileService.js
+++ src/domain/services/ProfileService.js
@@ -1,17 +1,20 @@
 import {status} from '../constants/Profile';
+import {API_BASE} from '../../SharedConstants';
 
-
-export const updateProfile = store => {
-    store.setState({ Status: status.init });
+export const updateProfile = async store => {
+    store.setState({ Status: status.loading });
     // perform chapter update
-    store.setState({ Status: status.ready });
+    return new Promise((resolve, reject) => {
+        store.setState({ Status: status.ready });
+        resolve();
+    });
 }
 
-export const loadProfile = async(store) => {
+export const loadProfile = async store => {
     store.setState({ Status: status.loading });
     // call Chapter API
     return new Promise((resolve, reject) => {
-        store.setState({ Status: status.loading });
+        store.setState({ Status: status.ready });
         resolve();
     });
 
