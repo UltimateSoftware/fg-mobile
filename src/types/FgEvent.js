64b5f696351b7fe7423cfea0e9255726bb2bea17
Jonathan Beltran <jonathanbeltran@Jonathans-MacBook-Pro.local>
2019-06-16 17:41:59 -0700

    rebaseing off of master

--- src/types/FgEvent.js
+++
@@ -1,9 +0,0 @@
-export function FgEvent (title, description, date, state, location, chapterID, icebreakers) {
-  this.title = title;
-  this.description = description;
-  this.date = date;
-  this.state = state;
-  this.location = location;
-  this.chapterID = chapterID;
-  this.icebreakers = icebreakers;
-}
