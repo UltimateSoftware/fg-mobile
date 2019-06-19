e91aa7391af8dd3a92b3a63c0a9e5fb5b7a96849
Ryan Stern <ryan_stern@ultimatesoftware.com>
2019-06-19 09:15:29 -0400

    added divs for each section of chapter page

--- src/pages/FgChapter.js
+++ src/pages/FgChapter.js
@@ -5,6 +5,7 @@ import {Inspiration} from '../components/atoms/Inspiration';
 import {Banner} from '../components/atoms/Banner';
 import {ProfileFrame} from '../components/primatives/ProfileFrame';
 import useChapter from '../domain/models/Chapter';
+import {ProfileBanner} from '../components/molecules/ProfileBanner';
 
 function oneOff(){
 
@@ -17,7 +18,8 @@ function FgChapter() {
     // any new information which the backend provides needs to be reflected in domain/models/Chapter
     const {Chapter, Status} = chapter; // Use Chapter to object to populate page
     
-    const imgUri = 'fearlesslyGirl_logo.jpg';
+    // const imgUri = 'fearlesslyGirl_logo.jpg';
+    const imgUri = <ProfileBanner />;
     
     return (
         <ScrollView>
@@ -26,7 +28,27 @@ function FgChapter() {
                 <Banner color={"#6ED4C8"} source={imgUri}> 
                     <ProfileFrame source={imgUri} size={'l'}/>
                 </Banner>
-                <Inspiration title={"my title"} inspiration={"some text"}/>
+                <Inspiration title={"Our history"} inspiration={"some text"}/>
+                <Inspiration title={"Student leadership"} inspiration={"some text"}/>
+                <Inspiration title={"Our mission"} inspiration={"some text"}/>
+                <Inspiration title={"FearlesslyGirl bylaws"} inspiration={
+                    <>
+                        <Text>I. Lorem Ipsum</Text>
+                        <Text>II. Lorem Ipsum</Text>
+                        <Text>III. Lorem Ipsum</Text>
+                        <Text>IV. Lorem Ipsum</Text>
+                        <Text>V. Lorem Ipsum</Text>
+                    </>
+                }/>
+                <Inspiration title={"Chapter bylaws"} inspiration={
+                    <React.Fragment>
+                    <Text>I. Lorem Ipsum</Text>
+                    <Text>II. Lorem Ipsum</Text>
+                    <Text>III. Lorem Ipsum</Text>
+                    <Text>IV. Lorem Ipsum</Text>
+                    <Text>V. Lorem Ipsum</Text>
+                </React.Fragment>
+                }/>
                 {/* NEED TO RENDER PHOTO OF CHAPTER MEMBER */}
             </View>
         </ScrollView>
