66979bb5e94c2dc3a496e857e075deb6a56806ef
lauren_diaz <lauren_diaz@ultimatesoftware.com>
2019-06-19 12:30:05 -0400

    upated agenda appearance

--- src/components/atoms/CalendarItem.js
+++ src/components/atoms/CalendarItem.js
@@ -3,15 +3,37 @@ import React, { Component } from 'react';
 import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
 import {SCREEN_HEIGHT, SCREEN_WIDTH, BANNER_HEIGHT_WIDTH_RATIO } from "../../SharedConstants";
 
-
-export function CalendarItem({text}) {
+// pass in event also with text
+export function CalendarItemPopulated({text}) {
     return (
-        <View>
+        // click instead of view
+        <View style={styles.item}>
             <Text>{text}</Text>
         </View>);
 }
 
+export function CalendarItemEmpty({text}) {
+    // return (<View style={styles.emptyDate}/>)
+    return (<View style={styles.item}/>)
+}
 
 const styles = StyleSheet.create({
-    
+    item: {
+        backgroundColor: 'white',
+        flex: 1,
+        borderRadius: 5,
+        padding: 10,
+        marginRight: 10,
+        marginTop: 17,
+        minHeight: 60
+    },
+    // need ?????
+    // emptyDate: {
+    //     height: 35,
+    //     flex: 1,
+    //     paddingTop: 30,
+    //     backgroundColor: 'white',
+    //     borderRadius: 5,
+    //     marginTop: 17
+    // }
 });
