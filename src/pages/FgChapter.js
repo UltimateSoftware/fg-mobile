894b513e626843aa524cdff2eeff0c9702264c7e
Ryan Stern <ryan_stern@ultimatesoftware.com>
2019-06-19 11:16:37 -0400

    removed inspirations and added views instead

--- src/pages/FgChapter.js
+++ src/pages/FgChapter.js
@@ -6,6 +6,7 @@ import {Banner} from '../components/atoms/Banner';
 import {ProfileFrame} from '../components/primatives/ProfileFrame';
 import useChapter from '../domain/models/Chapter';
 import {ProfileBanner} from '../components/molecules/ProfileBanner';
+import {ParagraphBlock} from '../components/primatives/ParagraphBlock';
 
 function oneOff(){
 
@@ -28,28 +29,66 @@ function FgChapter() {
                 <Banner color={"#6ED4C8"} source={imgUri}> 
                     <ProfileFrame source={imgUri} size={'l'}/>
                 </Banner>
-                <Inspiration title={"Our history"} inspiration={"some text"}/>
-                <Inspiration title={"Student leadership"} inspiration={"some text"}/>
-                <Inspiration title={"Our mission"} inspiration={"some text"}/>
-                <Inspiration title={"FearlesslyGirl bylaws"} inspiration={
-                    <>
-                        <Text>I. Lorem Ipsum</Text>
-                        <Text>II. Lorem Ipsum</Text>
-                        <Text>III. Lorem Ipsum</Text>
-                        <Text>IV. Lorem Ipsum</Text>
-                        <Text>V. Lorem Ipsum</Text>
-                    </>
-                }/>
-                <Inspiration title={"Chapter bylaws"} inspiration={
-                    <React.Fragment>
-                    <Text>I. Lorem Ipsum</Text>
-                    <Text>II. Lorem Ipsum</Text>
-                    <Text>III. Lorem Ipsum</Text>
-                    <Text>IV. Lorem Ipsum</Text>
-                    <Text>V. Lorem Ipsum</Text>
-                </React.Fragment>
-                }/>
-                {/* NEED TO RENDER PHOTO OF CHAPTER MEMBER */}
+                <Inspiration title={"Chapter name"} inspiration={"Chapter number"}/>
+
+                <View style={styles.paragraphs}>
+                    <View style={[styles.inspirationTitle, {marginTop: 20}]}>
+                        <View style={styles.inspirationLine}/>
+                            <Text style={styles.inspirationLabel}>  Our history  </Text>
+                        <View style={styles.inspirationLine}/>
+                    </View>
+                    <Text style={{padding: '5%'}}>Lorem Ipsum</Text>
+                </View>
+
+                <View >
+                    <View style={[styles.inspirationTitle, {marginTop: 20}]}>
+                        <View style={styles.inspirationLine}/>
+                            <Text style={styles.inspirationLabel}>  Student leadership  </Text>
+                        <View style={styles.inspirationLine}/>
+                    </View>
+                    <View>
+                    <Text style={{padding: '5%'}}>Lorem Ipsum</Text>
+                    </View>
+                </View>
+
+                <View style={styles.paragraphs}>
+                    <View style={[styles.inspirationTitle, {marginTop: 20}]}>
+                        <View style={styles.inspirationLine}/>
+                            <Text style={styles.inspirationLabel}>  Our mission  </Text>
+                        <View style={styles.inspirationLine}/>
+                    </View>
+                    <Text style={{padding: '5%'}}>Lorem Ipsum</Text>
+                </View>
+
+                <View style={styles.paragraphs}>
+                    <View style={[styles.inspirationTitle, {marginTop: 20}]}>
+                        <View style={styles.inspirationLine}/>
+                            <Text style={styles.inspirationLabel}>  FearlesslyGirl bylaws  </Text>
+                        <View style={styles.inspirationLine}/>
+                    </View>
+                    <View>
+                        <Text style={styles.textList}>I. Lorem Ipsum</Text>
+                        <Text style={styles.textList}>II.Lorem Ipsum</Text>
+                        <Text style={styles.textList}>III. Lorem Ipsum</Text>
+                        <Text style={styles.textList}>IV. Lorem Ipsum</Text>
+                        <Text style={styles.textList}>V. Lorem Ipsum</Text>
+                    </View>
+                </View>
+
+                <View style={styles.paragraphs}>
+                    <View style={[styles.inspirationTitle, {marginTop: 20}]}>
+                        <View style={styles.inspirationLine}/>
+                            <Text style={styles.inspirationLabel}>  Chapter bylaws  </Text>
+                        <View style={styles.inspirationLine}/>
+                    </View>
+                    <View >
+                        <Text style={styles.textList}>I. Lorem Ipsum</Text>
+                        <Text style={styles.textList}>II.Lorem Ipsum</Text>
+                        <Text style={styles.textList}>III. Lorem Ipsum</Text>
+                        <Text style={styles.textList}>IV. Lorem Ipsum</Text>
+                        <Text style={styles.textList}>V. Lorem Ipsum</Text>
+                    </View>
+                </View>
             </View>
         </ScrollView>
     );
@@ -74,6 +113,35 @@ const styles = StyleSheet.create({
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
+        },
+        inspirationTitle: {
+            flexDirection: 'row',
+            alignContent: 'center',
+            justifyContent: 'center'
+        },
+        inspirationLabel: {
+            fontFamily: 'montserrat-regular',
+            fontSize: 18,
+            color: '#818282'
+        },
+        inspirationLine: {
+            position: 'relative',
+            borderBottomColor:'#818282',
+            borderBottomWidth:1,
+            height:'60%',
+            width:'32%'
+        },
+        paragraphs: {
+            flex: 1,
+            // alignItems: "center",
+            justifyContent: "flex-start"
+        },
+        textList: {
+            fontWeight: "bold",
+            color: 'gray',
+            fontSize: 18,
+            padding: '3%',
+            marginLeft: '3%'
         }
 });
 
