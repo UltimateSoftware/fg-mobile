7dad30eb93a519100cc139a18b66dea5302f480f
Chandler Moeller <chandler_moeller@ultimatesoftware.com>
2019-06-28 11:04:07 -0400

    feature/ProfileBanner (feat): add view all button and functionality to fgProfile

--- src/pages/FgProfile.js
+++ src/pages/FgProfile.js
@@ -14,9 +14,12 @@ function FgProfile() {
     // See Profile definition in domain/models/Profile
     // any new information which the backend provides needs to be reflected in domain/models/Profile
     const {Profile, Status} = profile; // Use Profile to object to populate page
-    
+
+    const [viewAll, setViewAll] = useState(false);
+
     const imgUri = 'fearlesslyGirl_logo.jpg';
-    var members = [{name: "test1", source: "fearlesslyGirl_logo.jpg"}, {name: "test2", source: "fearlesslyGirl_logo.jpg"}, {name: "test3", source: "fearlesslyGirl_logo.jpg"}, {name: "test4", source: "fearlesslyGirl_logo.jpg"}, {name: "test5", source: "fearlesslyGirl_logo.jpg"}, {name: "test6", source: "fearlesslyGirl_logo.jpg"}]
+    const allMembers = [{name: "test1", source: "fearlesslyGirl_logo.jpg"}, {name: "test2", source: "fearlesslyGirl_logo.jpg"}, {name: "test3", source: "fearlesslyGirl_logo.jpg"}, {name: "test4", source: "fearlesslyGirl_logo.jpg"}, {name: "test5", source: "fearlesslyGirl_logo.jpg"}, {name: "test6", source: "fearlesslyGirl_logo.jpg"}]
+    const [members, setMembers] = useState(allMembers.slice(0,4));
     var renderMembers = []
 
     _renderMember = (member, i) => (
@@ -25,14 +28,27 @@ function FgProfile() {
             <Text style={[styles.textContainer, styles.nameLabel]}>{member.name}</Text>
         </View>
     )
-    
+
+    const handleButton = () => {
+        setViewAll(true)
+        setMembers(allMembers);
+    };
+
+    button = !viewAll ?
+    <Button onPress={event => handleButton()} title="View All"></Button> : null;
+
     return (
         <ScrollView>
             <View style={styles.container}>
                 <ProfileBanner backImgUri={imgUri} imgUri={imgUri} lineOneText="test" lineTwoText="Thre" lineThreeText="t"/>
                 <Inspiration title={"Inspiration"} inspiration={"lorem ipsum test text messages"}/>
-                <Inspiration title={"Chapter Sisters"} inspiration={""} parMar={-40}/>
+                <View style={[styles.titleView, {marginTop: 20}]}>
+                    <View style={styles.titleLine}/>
+                    <Text style={styles.titleLabel}>Chapter Sisters</Text>
+                    <View style={styles.titleLine}/>
+                </View>
                 <Grid style={styles.list} renderItem={this._renderMember} keyExtractor={(item, index) => index.toString()} data={members} refreshing= {false} numColumns={4}/>
+                {button}
 
             </View>
         </ScrollView>
@@ -53,16 +69,13 @@ const styles = StyleSheet.create({
         justifyContent: 'center',
         alignItems: 'center',
         backgroundColor: 'white',
-        },
+    },
     title: {
         fontSize: 20,
         textAlign: 'center',
         margin: 10,
         },
     member: {
-        // justifyContent: 'space-between',
-        // flexDirection: 'row',
-        // alignItems: 'flex-start',
         margin: 7,
     },
     list: {
@@ -71,10 +84,28 @@ const styles = StyleSheet.create({
     nameLabel: {
         fontFamily: 'montserrat-light',
         fontSize: 18,
-      },
+    },
     textContainer: {
         color: '#818282',
         textAlign: 'center',
-      },
+    },
+    titleView: {
+        flexDirection: 'row',
+        alignContent: 'center',
+        justifyContent: 'center',
+        paddingBottom: 15
+    },
+    titleLabel: {
+        fontFamily: 'montserrat-regular',
+        fontSize: 18,
+        color: '#818282'
+    },
+    titleLine: {
+        position: 'relative',
+        borderBottomColor:'#818282',
+        borderBottomWidth:1,
+        height:'60%',
+        width:'32%'
+    }
 });
 
