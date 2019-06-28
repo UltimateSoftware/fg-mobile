a98204cb2a9f015353d21a214c487b2c5b277e69
lauren_diaz <lauren_diaz@ultimatesoftware.com>
2019-06-28 11:24:24 -0400

    added button w no functionality

--- src/components/atoms/CalendarItem.js
+++ src/components/atoms/CalendarItem.js
@@ -1,24 +1,89 @@
 
 import React, { Component } from 'react';
-import {StyleSheet, View, Text, Image, TouchableOpacity} from 'react-native';
+import {StyleSheet, View, Text, Button} from 'react-native';
 import {SCREEN_HEIGHT, SCREEN_WIDTH, BANNER_HEIGHT_WIDTH_RATIO } from "../../SharedConstants";
+import { useState, useEffect } from 'react';
 
-// pass in event also with text
 export function CalendarItemPopulated({text}) {
+    // init state=false bc not expanded
+    // const [isExpanded, expandItem] = useState(false);
+    // changeBtn((isExpanded) => {
+    //     if(true) {
+             
+    //     } else {
+    //         <Button title = "+"/>
+    //     }
+    // })
     return (
-        // click instead of view
-        <View style={styles.item}>
-            <Text>{text}</Text>
-        </View>);
+        <View style={styles.container}>
+            <View style={styles.txtbox}>
+                <Text>{text}</Text>
+            </View>
+            <View style={styles.btnbox}>
+                <Button
+                    title="+"
+                    onPress={() => {
+                        return expandItem(!isExpanded);
+                    }}
+                />
+                    
+                {/* </Button> */}
+            </View>
+        </View>
+        );
 }
 
-export function CalendarItemEmpty({text}) {
-    // return (<View style={styles.emptyDate}/>)
+
+// modal ?
+// const AgendaItem = createStackNavigator({
+//     Main: {
+//         screen: FgEvents
+//     },
+//     EventForm: {
+//         screen: EventFormScreen
+//     }},
+//     {
+//         mode: 'modal'
+//      },
+//  )
+
+
+
+
+
+// handle = (state) => {
+//     console.log("test handle");
+//     if(state == true) {
+//         title = "-";
+//         console.log(title);
+//     } else {
+//         title = "+";
+//         console.log(title);
+//     }
+//     return !state;
+// }
+
+// export function setState(state) {
+//     if(state == true) {
+//         title = "+"
+//     } else {
+//         title = "-"
+//     }
+//     return !state;
+// }
+
+// export function handle() {
+//     //
+//     return();
+// }
+
+export function CalendarItemEmpty() {
     return (<View style={styles.item}/>)
 }
 
 const styles = StyleSheet.create({
-    item: {
+    container: {
+        flexDirection: 'row',
         backgroundColor: 'white',
         flex: 1,
         borderRadius: 5,
@@ -27,6 +92,17 @@ const styles = StyleSheet.create({
         marginTop: 17,
         minHeight: 60
     },
+    txtbox: {
+        //
+    },
+    btnbox: {
+        flex: 1,
+        alignItems: 'flex-end',
+        textAlign: 'right'
+        // flex: 1,
+        // // justifyContent: 'flex-end'
+        // alignItems: 'right'
+    }
     // need ?????
     // emptyDate: {
     //     height: 35,
--- src/pages/FgEvents.js
+++ src/pages/FgEvents.js
@@ -5,9 +5,11 @@ import useEvents from '../domain/models/Event';
 
 import HamburgerIcon from '../components/primatives/HamburgerIcon';
 import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../SharedConstants';
-import { CalendarItemPopulated, CalendarItemEmpty } from '../components/atoms/CalendarItem';
+import { CalendarItemPopulated, CalendarItemEmpty, ExpandItem, setState } from '../components/atoms/CalendarItem';
+import TouchableHighlight from 'react-native-gesture-handler';
 
 function FgEvents() {
+    console.log("test 3");
     const [events, eventActions] = useEvents()
     const {Event, Status} = events;
     let date = new Date();
@@ -18,26 +20,59 @@ function FgEvents() {
         // fmt for title, time, desc
         // multi-day events
         items={{
-            '2019-06-16': [],
-            '2019-06-17': [{text: 'item 17 - any js object'}],
-            '2019-06-18': [{text: 'item 18 - any js object'}],
+            '2019-06-23': [],
+            '2019-06-24': [{text: 'item 17 - any js object'}],
+            '2019-06-25': [{text: 'item 18 - any js object'}],
             // '`${date.getFullYear()}`-`${date.getMonth()}`-`${date.getDate()}`': [{text: 'item 1 - any js object'}],
-            '2019-06-19': [{text: 'item 19.1 - any js object'}, {text: 'item 19.2 - another one'}, {text: 'item 19.3 - another one'}, {text: 'item 19.4 - another one'}, {text: 'item 19.5 - another one'}],
-            '2019-06-20': [{text: 'item 20 - any js object'}],
-            '2019-06-21': [],
+            '2019-06-26': [{text: 'item 19.1 - any js object'}, {text: 'item 19.2 - another one'}, {text: 'item 19.3 - another one'}, {text: 'item 19.4 - another one'}, {text: 'item 19.5 - another one'}],
+            '2019-06-27': [{text: 'item 20 - any js object'}],
+            '2019-06-28': [],
             '2019-06-22': [{text: 'item 22 - any js object'}]
         }}
 
+        /*
+        an event object
+        let event = {
+            title = String,
+            startTime: String,
+            endTime: String,
+            startDate: String,
+            endDate: String,
+            location: String,
+            description: String,
+        }
+
+        let events = event[];
+        */
+
+        /*
+        an event has: 
+        - start time
+        - end time (?)
+        - start date
+        - end date (?)
+        - title
+        - description/extra info
+
+        sort by date, then start time
+
+        */
+
         renderItem={(item, firstItemInDay) => {
-            return(<CalendarItemPopulated text={item.text}/>);
+            console.log("test 2");
+            return(<CalendarItemPopulated text={item.text} />);
         }}
+
         renderEmptyDate={() => {
+            // insert graphic line or something
             return(<CalendarItemEmpty/>);
         }}
+
         rowHasChanged={(r1, r2) => {
             return r1.text !== r2.text
         }}
         />
+        
         </View>
     );
 }
@@ -54,17 +89,11 @@ export default FgEvents
 const styles = StyleSheet.create({
     container: {
         height: '100%',
-        backgroundColor: 'pink'
-        // flex: 2,
-        // justifyContent: 'center',
-        // alignItems: 'center',
-        // backgroundColor: 'white',
-        // margin: 0
         },
-    title: {
-        fontSize: 20,
-        textAlign: 'center',
-        margin: 10,
-        }
+    // title: {
+    //     fontSize: 20,
+    //     textAlign: 'center',
+    //     margin: 10,
+    //     }
 });
 
