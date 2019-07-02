0f06cc656d873fbe0320f1e6975929d740c2be7f
lauren_diaz <lauren_diaz@ultimatesoftware.com>
2019-07-02 14:42:08 -0400

    agenda item components

--- src/pages/FgEvents.js
+++ src/pages/FgEvents.js
@@ -20,30 +20,15 @@ function FgEvents() {
         // fmt for title, time, desc
         // multi-day events
         items={{
-            '2019-06-23': [],
-            '2019-06-24': [{text: 'item 17 - any js object'}],
-            '2019-06-25': [{text: 'item 18 - any js object'}],
+            '2019-06-27': [],
+            '2019-06-28': [{text: 'item 17 - any js object'}],
+            '2019-06-29': [{text: 'item 18 - any js object'}],
             // '`${date.getFullYear()}`-`${date.getMonth()}`-`${date.getDate()}`': [{text: 'item 1 - any js object'}],
-            '2019-06-26': [{text: 'item 19.1 - any js object'}, {text: 'item 19.2 - another one'}, {text: 'item 19.3 - another one'}, {text: 'item 19.4 - another one'}, {text: 'item 19.5 - another one'}],
-            '2019-06-27': [{text: 'item 20 - any js object'}],
-            '2019-06-28': [],
-            '2019-06-22': [{text: 'item 22 - any js object'}]
+            '2019-06-30': [{text: 'item 19.1 - any js object'}, {text: 'item 19.2 - another one'}, {text: 'item 19.3 - another one'}, {text: 'item 19.4 - another one'}, {text: 'item 19.5 - another one'}],
+            '2019-07-01': [{text: 'item 20 - any js object'}],
+            '2019-07-02': [],
+            '2019-07-03': [{text: 'item 22 - any js object'}]
         }}
-
-        /*
-        an event object
-        let event = {
-            title = String,
-            startTime: String,
-            endTime: String,
-            startDate: String,
-            endDate: String,
-            location: String,
-            description: String,
-        }
-
-        let events = event[];
-        */
 
         /*
         an event has: 
@@ -135,15 +120,9 @@ const newEventPage = createStackNavigator({
 export default newEventPage;
 // export default FgEvents
 
-// ??????? fix
 const styles = StyleSheet.create({
     container: {
         height: '100%',
         },
-    // title: {
-    //     fontSize: 20,
-    //     textAlign: 'center',
-    //     margin: 10,
-    //     }
 });
 
