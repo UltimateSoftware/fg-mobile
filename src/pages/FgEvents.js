8914356345f326294dfecc4ef0dfa0716af532d6
lauren_diaz <lauren_diaz@ultimatesoftware.com>
2019-07-03 12:57:15 -0400

    rm android/ & removed comments

--- src/pages/FgEvents.js
+++ src/pages/FgEvents.js
@@ -1,47 +1,36 @@
-import React, {Component, useState} from 'react';
-import {Platform, StyleSheet, Text, View} from 'react-native';
-import {Calendar, CalendarList, Agenda} from 'react-native-calendars';
+import React from 'react';
+import {StyleSheet, View} from 'react-native';
+import {Agenda} from 'react-native-calendars';
 import useEvents from '../domain/models/Event';
 
 import HamburgerIcon from '../components/primatives/HamburgerIcon';
-import { SCREEN_WIDTH, SCREEN_HEIGHT } from '../SharedConstants';
-import { CalendarItemPopulated, CalendarItemEmpty, ExpandItem, setState } from '../components/atoms/CalendarItem';
-import TouchableHighlight from 'react-native-gesture-handler';
+import { CalendarItemPopulated, CalendarItemEmpty } from '../components/atoms/CalendarItem';
 
 function FgEvents() {
-    console.log("test 3");
     const [events, eventActions] = useEvents()
     const {Event, Status} = events;
     let date = new Date();
     return (
         <View style={styles.container}>
         
-        <Agenda 
-        // fmt for title, time, desc
-        // multi-day events
+        <Agenda
         items={{
             '2019-06-27': [],
             '2019-06-28': [{text: 'item 17 - any js object'}],
             '2019-06-29': [{text: 'item 18 - any js object'}],
-            // '`${date.getFullYear()}`-`${date.getMonth()}`-`${date.getDate()}`': [{text: 'item 1 - any js object'}],
             '2019-06-30': [{text: 'item 19.1 - any js object'}, {text: 'item 19.2 - another one'}, {text: 'item 19.3 - another one'}, {text: 'item 19.4 - another one'}, {text: 'item 19.5 - another one'}],
             '2019-07-01': [{text: 'item 20 - any js object'}],
             '2019-07-02': [],
-            '2019-07-03': [{text: 'item 22 - any js object'}]
+            '2019-07-03': [{text: 'item 22 - any js object'}],
+            '2019-07-04': [{text: 'item 19.1 - any js object'}, {text: 'item 19.2 - another one'}, {text: 'item 19.3 - another one'}, {text: 'item 19.4 - another one'}, {text: 'item 19.5 - another one'}],
+            '2019-07-05': [{text: 'item 20 - any js object'}],
+            '2019-07-06': [],
+            '2019-07-07': [{text: 'item 22 - any js object'}],
+            '2019-07-08': [{text: 'item 19.1 - any js object'}, {text: 'item 19.2 - another one'}, {text: 'item 19.3 - another one'}, {text: 'item 19.4 - another one'}, {text: 'item 19.5 - another one'}],
+            '2019-07-09': [{text: 'item 20 - any js object'}],
+            '2019-07-10': [],
+            '2019-07-11': [{text: 'item 22 - any js object'}]
         }}
-
-        /*
-        an event has: 
-        - start time
-        - end time (?)
-        - start date
-        - end date (?)
-        - title
-        - description/extra info
-
-        sort by date, then start time
-
-        */
 
         renderItem={(item, firstItemInDay) => {
             console.log("test 2");
@@ -49,13 +38,12 @@ function FgEvents() {
         }}
 
         renderEmptyDate={() => {
-            // insert graphic line or something
             return(<CalendarItemEmpty/>);
         }}
 	
-	renderEmptyData={() => {
-		return null;
-	}}
+        renderEmptyData={() => {
+            return null;
+        }}
 
         rowHasChanged={(r1, r2) => {
             return r1.text !== r2.text
