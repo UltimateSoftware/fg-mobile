81ba16e6000af1b4d7244c1099bb278f624456bd
AmeliaM <ameliameles@gmail.com>
2019-07-17 22:50:51 -0400

    deleted commented out code and auth token

--- src/domain/services/HangoutService.js
+++ src/domain/services/HangoutService.js
@@ -1,6 +1,6 @@
 import {status} from '../constants/Hangout';
 import {API_BASE} from '../../SharedConstants';
-const auth = "Bearer eyJ0eXAiOiJKV1QiLCJhbGciOiJSUzI1NiIsImtpZCI6IlJUazROVE15UVRZMU5EbENNVGN4TURJNE1qQXhSa1pETmtReU1FSTFPRFJHUmpWQk1qRTVNUSJ9.eyJpc3MiOiJodHRwczovL2ZlYXJsZXNzbHlnaXJsLmF1dGgwLmNvbS8iLCJzdWIiOiJxeXdScjFYYTVxYkFrclhLNVhKazZjRko2dkdDZUl0ekBjbGllbnRzIiwiYXVkIjoib2F1dGgvdG9rZW4iLCJpYXQiOjE1NjI4NjAzNTksImV4cCI6MTU2Mjk0Njc1OSwiYXpwIjoicXl3UnIxWGE1cWJBa3JYSzVYSms2Y0ZKNnZHQ2VJdHoiLCJndHkiOiJjbGllbnQtY3JlZGVudGlhbHMifQ.da_dZmympb8CS2Vk517GofWIOGv73g1bu_LGgYsEeuRFOx_DIbE2nZ4tBdmljhvqZ3wvbo7Yki0yvFSwMyRp2eTotjMuHMffvJApwi4uohBXZmCgpSnJV6M6c84MgTvYEnVTcHvkDtuovGw-L73GGy6zQrt8aZ7SKnPMBrgKOetd5M4knEXMZQOlY-SouXS5UeGpbG7k3Uo2pMc8GPuZIg-JZwbDjDpi_0z2qBO6ArAuu6W3L_ME-nOaI__hsLYygThOOWz_9h_FwGXs57VPSN0beNGcM36fMU_USVDCY8lc_NYWN_6tteES-s_2eMXZO_LMbweR6sDePxR48ChIng";
+const auth = "";
 export const createHangout = async store => {
     store.setState({ Status: status.loading });
     // perform hangout creation
