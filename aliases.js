d3e6ffce96adb405b4204562caa7a4881e2de939
arcticfaded <jbelt021@fiu.edu>
2019-10-03 09:04:29 -0400

    partial update to aliases

---
+++ aliases.js
@@ -0,0 +1,7 @@
+module.exports = {
+    '@components': './src/components',
+    '@config': './src/config',
+    '@hooks': './src/domain',
+    '@pages': './src/pages',
+    '@assets': './assets',
+  };
--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,15 @@
+const alias = require('./aliases');
+
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  plugins: [
+    [
+      require.resolve('babel-plugin-module-resolver'),
+      {
+        alias,
+        root: ['./'],
+        extensions: [".ios.js", ".android.js", ".js", ".json"]
+      }
+    ],
+  ]
 };
